d5e0ab9ca3f12e1d98ca9845a75c5d02bd24640f
Rick <acab310501@gmail.com>
2026-08-07 16:20:07 +0300

    Add GitHub Actions workflow for build and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Flora_Atelier/', // <--- Добавлена эта строка! (название совпадает с репозиторием)
   resolve: {
     tsconfigPaths: true,
   },
