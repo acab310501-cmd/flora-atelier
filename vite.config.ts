7123cd73b84727d6433a7a2213422ab2b1bf23fd
Rick <acab310501@gmail.com>
2026-08-07 19:42:51 +0300

    Первый коммит с проектом

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // Важно! Если ваш репозиторий называется не acab310501-cmd.github.io, а просто flora-atelier,
-  // то раскомментируйте строку ниже и укажите название репозитория:
-  // base: '/flora-atelier/', 
+  // Важно! Указываем название репозитория, чтобы GitHub Pages знал, где искать файлы.
+  base: '/flora-atelier/', 
 });
