cc6e9033ed58491340d9771938ebe2b9377a7753
Rick <acab310501@gmail.com>
2026-08-07 18:56:28 +0300

    fix: add vercel.json and fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import path from 'path'; // Импортируйте path, если используете алиасы
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    tsconfigPaths: true,
+    // Если вы хотите использовать алиас @/ для src, то правильно писать так:
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
